89347bd50bd3eb933b610962e229886de68df715
nikoris77 <nfalba@gmail.com>
2014-06-10 13:30:29 -0500

    Initial Commit

---
+++ You'reInJeopardy/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  You'reInJeopardy
+//
+//  Created by Nicholas Falba on 6/10/14.
+//  Copyright (c) 2014 Nicholas Falba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NRFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NRFAppDelegate class]));
+    }
+}
---
+++ You'reInJeopardyTests/You_reInJeopardyTests.m
@@ -0,0 +1,34 @@
+//
+//  You_reInJeopardyTests.m
+//  You'reInJeopardyTests
+//
+//  Created by Nicholas Falba on 6/10/14.
+//  Copyright (c) 2014 Nicholas Falba. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface You_reInJeopardyTests : XCTestCase
+
+@end
+
+@implementation You_reInJeopardyTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
